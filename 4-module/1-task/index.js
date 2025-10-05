98089c4338d07f87703605fdad51a41c310d4eca
Maria Titova <titova@postmodern.ru>
2025-10-05 23:45:50 +0300

    1,2 tasks 4 lesson

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,14 @@
 function makeFriendsList(friends) {
-  // ваш код...
+
+  let friendsNames = friends.map((item) => `${item.firstName} ${item.lastName}`);
+
+  let ul1 = document.createElement('ul');
+
+  for(item in friendsNames) {
+    let li = document.createElement('li');
+    li.textContent = friendsNames[item];
+    ul1.append(li);
+  }
+
+  return ul1;
 }
--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,11 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let rows = table.rows;
+    for(let i = 0; i< rows.length;i++){
+      let cells = rows[i].cells;
+      for(let j = 0; j<cells.length;j++) {
+        if (i === j) {
+          cells[j].style.backgroundColor = 'red';
+        }
+      }
+    }
 }
