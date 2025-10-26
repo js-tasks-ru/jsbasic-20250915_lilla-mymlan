55f18f4350c1de0d6536e9f166fddd57a53a4bc6
Maria Titova <titova@postmodern.ru>
2025-10-27 00:01:59 +0300

    1,2,3,4 tesks 7 module

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -3,5 +3,97 @@ import createElement from '../../assets/lib/create-element.js';
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    this._elem = this.render();
+  }
+  
+  render() {
+    const categories = this.categories
+    .map((cat) => 
+    `<a href="#" class="ribbon__item" data-id="${cat.id}">${cat.name}</a>`
+    )
+    .join('');
+
+    const div = createElement(`
+      <div class="ribbon">
+      <!--Кнопка прокрутки влево-->
+      <button class="ribbon__arrow ribbon__arrow_left">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>
+
+      <!--Ссылки на категории-->
+      <nav class="ribbon__inner">
+        ${categories}
+      </nav>
+
+      <!--Кнопка прокрутки вправо-->
+      <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>
+    </div>
+    `)
+    
+    this.onClickBtn(div);
+    this.onScroll(div);
+    this.onClickCategory(div);
+
+    return div;
+  }
+
+  onClickBtn(div) {
+    let ribbon = div;
+    function arrowsLeftRight(event) {
+      let ribbonInner = div.querySelector('.ribbon__inner');
+      let arrowLeft = event.target.closest('.ribbon__arrow_left');
+      let arrowRight = event.target.closest('.ribbon__arrow_right');
+
+      if(arrowLeft) ribbonInner.scrollBy(-350,0);
+      if(arrowRight) ribbonInner.scrollBy(350,0);
+    }
+
+    ribbon.addEventListener('click',arrowsLeftRight);
+
+  }
+
+  onScroll(div) {
+    let ribbonInner = div.querySelector('.ribbon__inner');
+
+    ribbonInner.addEventListener('scroll',() => {
+        let arrowLeft = div.querySelector('.ribbon__arrow_left');
+        let arrowRight = div.querySelector('.ribbon__arrow_right');
+        
+        let scrollLeft = ribbonInner.scrollLeft;
+        let scrollWidth = ribbonInner.scrollWidth;
+        let scrollClient = ribbonInner.clientWidth;
+
+        let scrollRight = scrollWidth - scrollClient - scrollLeft;
+
+        (scrollLeft === 0) ? arrowLeft.classList.remove('ribbon__arrow_visible') : arrowLeft.classList.add('ribbon__arrow_visible');
+        (scrollRight < 1) ? arrowRight.classList.remove('ribbon__arrow_visible') : arrowRight.classList.add('ribbon__arrow_visible');
+      })
+  }
+
+  onClickCategory(div) {
+    let ribbonInner = div.querySelector('.ribbon__inner');
+    ribbonInner.addEventListener('click',(event) => {
+      let item = event.target.closest('.ribbon__item');
+      if(item) {
+        event.preventDefault();
+        let ribbons = ribbonInner.children;
+        for (let ribbon of ribbons) {
+          ribbon.classList.remove('ribbon__item_active');
+        };
+        item.classList.add('ribbon__item_active');
+        let ribbonSelect = new CustomEvent("ribbon-select", {
+        detail: item.dataset.id,
+        bubbles: true
+        });
+        item.dispatchEvent(ribbonSelect);
+        item.addEventListener('ribbon-select',() => {}) 
+      }
+    })
+  }
+
+  get elem() {
+    return this._elem;
   }
 }
--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,85 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.elem = this.render();
   }
+
+  render() {
+    this.elem = createElement(`
+      <div class="modal">
+      <!--Прозрачная подложка перекрывающая интерфейс-->
+      <div class="modal__overlay"></div>
+  
+      <div class="modal__inner">
+        <div class="modal__header">
+          <!--Кнопка закрытия модального окна-->
+          <button type="button" class="modal__close">
+            <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+          </button>
+  
+          <h3 class="modal__title">
+          </h3>
+        </div>
+  
+        <div class="modal__body">
+        </div>
+      </div>
+    </div>
+      `);
+
+    this.setTitle();
+    this.setBody();
+    this.onClick();
+    this.close();
+    this.onKey();
+
+    return this.elem;
+  }
+
+  open() {
+    document.body.classList.add('is-modal-open');
+
+    document.body.append(this.elem);
+  }
+
+  setTitle(titleText) {
+    let title = this.elem.querySelector('.modal__title');
+    title.textContent = titleText;
+  }
+
+  setBody(node) {
+    let body = this.elem.querySelector('.modal__body');
+    let lastNode = body.lastElementChild;
+    if(lastNode) lastNode.remove();
+    /*вставляется еще текст undefined, не могу понять, почему */
+    body.append(node);
+  }
+
+  onClick() {
+    let modalClose = this.elem.querySelector('.modal__close')
+    modalClose.addEventListener('click',() => {
+      document.body.classList.remove('is-modal-open');
+      this.elem.remove();
+      document.keydown = null;
+    });
+  }
+
+  close() {
+    document.body.classList.remove('is-modal-open');
+    this.elem.remove();
+    document.keydown = null;
+  }
+
+  onKey() {
+    let modal = this.elem;
+    function keyEsc(event) {
+      if(event.code === 'Escape') {
+        document.body.classList.remove('is-modal-open');
+        modal.remove();
+        document.keydown = null;
+      }
+    };
+    document.addEventListener('keydown',keyEsc);
+  }
+
 }
